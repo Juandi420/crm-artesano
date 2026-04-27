30742db64b5c1d003b9699c1bbb3d6a99c4e2524
Juandi420 <conexiapower@gmail.com>
2026-04-27 00:18:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const JWT_SECRET = 'tu-secreto-super-seguro-cambiar-en-produccion';
 
 // Middleware
